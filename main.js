46449b4404240049d83af3af9f8c907a32c171a0
FazleRifat <fazlerifatofficial@gmail.com>
2024-10-09 14:29:23 +0600

    propertise page done and consultants image change still not done

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 window.onload = function () {
   const bannerP1 = document.getElementById("banner-p-1");
   const bannerP2 = document.getElementById("banner-p-2");
+  const bannerP3 = document.getElementById("banner-p-3");
   const bannerP2_1 = document.getElementById("banner-p-2-1");
+  const bannerP3_1 = document.getElementById("banner-p-3-1");
   const speed = 20;
 
   const text1 = `Fed up with living out of a suitcase <br>
                   and waiting endlessly for home repairs?`;
   const text2 = `Join Our Team of  <br>
                   Independent <br>`;
+  const text3 = `Unlock Unique Buying  <br>
+                  Opportunities with <br>`;
   const text2_1 = `Expert Consultants `;
+  const text3_1 = `Fire-Sale Properties`;
 
   function typeWriter(element, text, index = 0, callback) {
     if (index < text.length) {
@@ -38,6 +43,16 @@ window.onload = function () {
       typeWriter(bannerP2, text2, 0, () => {
         // Once bannerP2 is finished, start typing for bannerP2_1
         typeWriter(bannerP2_1, text2_1, 0);
+      });
+    }, text1.length * speed); // Adjust delay based on text1 length
+  }
+  // Check if banner-p-3 exists
+  if (bannerP3) {
+    // Start typing for the second banner after a delay based on the length of text1
+    setTimeout(() => {
+      typeWriter(bannerP3, text3, 0, () => {
+        // Once bannerP2 is finished, start typing for bannerP2_1
+        typeWriter(bannerP3_1, text3_1, 0);
       });
     }, text1.length * speed); // Adjust delay based on text1 length
   }
